ea3e0d9c58bc7b60d1924f3bcfc48b25556b4c05
AamirAli Mir <mr.aamir.mir.4u@gmail.com>
2024-11-08 23:16:18 +0530

    Add ShadCN

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div>Moula Ali</div>;
+  return (
+    <main className="grid auto-rows-min gap-4 md:grid-cols-5">
+      {Array.from({ length: 20 }).map((_, i) => (
+        <div key={i} className="aspect-square rounded-xl bg-muted/50" />
+      ))}
+    </main>
+  );
 }
