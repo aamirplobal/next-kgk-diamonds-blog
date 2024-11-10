57ee0483f4cf062541e85d9511b23e979f5a2fa0
AamirAli Mir <mr.aamir.mir.4u@gmail.com>
2024-11-10 22:07:15 +0530

    Install prisma

---
+++ prisma/migrations/20241110154951_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT true,
+    "title" VARCHAR(255) NOT NULL,
+    "content" TEXT,
+    "imageUrl" VARCHAR(255),
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
